3cccccb6926917923c95f2a23457d5cf76e7f8a4
Alex Anderson <alexanderson1993@gmail.com>
2019-07-05 10:47:48 -0600

    Remove firebase, connect to CodeSandbox.

--- src/helpers/graphqlClient.js
+++ src/helpers/graphqlClient.js
@@ -2,20 +2,26 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { onError } from "apollo-link-error";
 import { setContext } from "apollo-link-context";
-import { ApolloLink } from "apollo-link";
+import { ApolloLink, split } from "apollo-link";
 import { createUploadLink } from "apollo-upload-client";
 import { ApolloProvider } from "react-apollo";
 import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import { WebSocketLink } from 'apollo-link-ws';
 
 import fetch from "isomorphic-fetch";
 
 import { auth } from "./firebase";
 import React from "react";
 
-const uri =
-  process.env.NODE_ENV === "production"
-    ? process.env.GATSBY_GRAPHQL_URL
-    : "http://localhost:5000/space-edventures-beta/us-central1/api/graphql";
+const uri = "https://18yw3.sse.codesandbox.io/";
+const wsUri ="wss://18yw3.sse.codesandbox.io/graphql"
+
+const wsLink = new WebSocketLink({
+  uri: wsUri,
+  options: {
+    reconnect: true
+  }
+});
 
 const AuthLink = setContext(async (_, { headers }) => {
   // get the authentication token from local storage if it exists
@@ -49,8 +55,21 @@ const DataLink = createUploadLink({
   credentials: "same-origin",
 });
 
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  ApolloLink.from([AuthLink, ErrorLink, DataLink]),
+);
+
 export const client = new ApolloClient({
-  link: ApolloLink.from([AuthLink, ErrorLink, DataLink]),
+  link,
   cache: new InMemoryCache(),
   fetch: typeof window !== "undefined" ? window.fetch : fetch,
 });
